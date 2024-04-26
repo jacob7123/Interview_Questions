debc540f385f809dcb453b0c499581d5546ac472
Chingyen Huang <h39713971@gmail.com>
2024-04-26 00:07:35 -0700

    Update Practice1.cpp

--- C&C++/Pointer/is_Value_Change/Practice1.cpp
+++ C&C++/Pointer/is_Value_Change/Practice1.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+void fn(int* p){
+    int* b = (int*)malloc(sizeof(int));
+    *b = 2;
+    p = b;
+}
+int main() {
+    // Write C++ code here
+    int a = 5;
+    int* p = &a;
+    cout << *p << endl;
+    fn(p);
+    cout << *p;
+    return 0;
+}
