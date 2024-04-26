6d2b8e5bffc8df6f3221de9f83ca46a1b010fc47
Chingyen Huang <h39713971@gmail.com>
2024-04-26 00:03:04 -0700

    Update main.cpp

--- C&C++/Sort_Stack_Using_Stack/main.cpp
+++ C&C++/Sort_Stack_Using_Stack/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <stack>
+using namespace std;
 
+int main(){
+  stack<int> s;
+
+  s.push(5);
+  s.push(3);
+  s.push(8);
+  s.push(1);
+  s.push(2);
+
+  cout << "Check original Stack:\n";
+  stack<int> tmp = s;
+  while(!s.empty()){
+    cout << tmp.top() << ' ';
+    tmp.pop();
+  }
+  
+}
